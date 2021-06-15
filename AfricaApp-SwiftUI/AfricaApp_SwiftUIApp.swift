9b1eb8b2f49724faad5370ca531369eaff4f0223
Justyna <kowalkowska.just@gmail.com>
2021-06-15 23:56:07 +0200

    First commit.

---
+++ AfricaApp-SwiftUI/AfricaApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp_SwiftUIApp.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
