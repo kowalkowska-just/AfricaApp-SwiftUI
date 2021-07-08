d2ca2e181ff0d0fab4c58e7f5a006c8b36f4224c
Justyna <kowalkowska.just@gmail.com>
2021-07-08 23:29:45 +0200

    Creating a CoverImageModel.

---
+++ AfricaApp-SwiftUI/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 08/07/2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
