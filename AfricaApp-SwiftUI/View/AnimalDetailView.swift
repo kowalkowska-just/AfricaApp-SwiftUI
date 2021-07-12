302837d6918451a53906a1349f2a23d1ee6cba0d
Justyna <kowalkowska.just@gmail.com>
2021-07-12 17:50:27 +0200

    Adding a descriptionView and factsView to AnimalDetailView.

--- AfricaApp-SwiftUI/View/AnimalDetailView.swift
+++ AfricaApp-SwiftUI/View/AnimalDetailView.swift
@@ -52,10 +52,25 @@ struct AnimalDetailView: View {
                 .padding(.horizontal)
                 
                 //FACTS
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+                    InsetFactView(animal: animal)
+                    
+                }
+                .padding(.horizontal)
+                
                 
                 //DESCRIPTIONS
+                Group {
+                    HeadingView(headingImage: "info.circle", headingText: "All about \(animal.name)")
+                    Text(animal.description)
+                        .multilineTextAlignment(.leading)
+                        .layoutPriority(1)
+                } //: Group
+                .padding(.horizontal)
                 
                 //MAP
+                
                 
                 //LINK
                 
