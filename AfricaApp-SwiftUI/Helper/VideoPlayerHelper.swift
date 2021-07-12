6831dcbdc7fe07fae44ab8491342cf79fb0d25f8
Justyna <kowalkowska.just@gmail.com>
2021-07-12 23:33:11 +0200

    Crating a VideoPlayerView.

---
+++ AfricaApp-SwiftUI/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 12/07/2021.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
