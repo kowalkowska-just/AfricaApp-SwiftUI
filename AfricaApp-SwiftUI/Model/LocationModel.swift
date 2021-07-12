26dc3178e785300ba94684211e55766277066b8d
Justyna <kowalkowska.just@gmail.com>
2021-07-13 00:03:48 +0200

    Creating a NationalParkLocation data model.

---
+++ AfricaApp-SwiftUI/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  AfricaApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 12/07/2021.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    //Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
